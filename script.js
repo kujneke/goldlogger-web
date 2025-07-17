802d36ae91724179ac6077f9c41cae53885d905c
kujneke <saso.lesnik@gmail.com>
2025-07-17 16:57:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const phrases = ["Discover Gold Veins", "Analyze Geological Data", "Optimize Your Finds"];
-let current = 0;
-const dynamicText = document.getElementById("dynamic-text");
-setInterval(() => {
-  current = (current + 1) % phrases.length;
-  dynamicText.textContent = phrases[current];
-}, 3000);
